b2d678d8085c1d3c0b21a9d72735721dd8dd6572
TcM1911 <mr.cableman@gmail.com>
2014-07-20 17:49:33 -0500

    Changed to AMD patched underscore.js and backbone.js.

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@
 
 // jQuery is already loaded because it's needed for bootstrap (frontend).
 require.config({
-	paths: {
-		underscore: '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min.js',
-		backbone: '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min.js'
+	paths: { 
+		backbone: 'js/lib/backbone-amd/backbone-min.js', // Patched for AMD
+		underscore: 'js/lib/underscore-amd/underscore-min.js' // Patched for AMD
 	}
 });
 
