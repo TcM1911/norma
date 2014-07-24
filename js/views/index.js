8969ae4947194fb64689b80d812e542624dc2a94
tcm1911 <mr.cableman@gmail.com>
2014-07-23 22:33:52 -0500

    Made the page more dynamic.

--- js/views/index.js
+++ js/views/index.js
@@ -1,17 +1,29 @@
+//Filename: books.js
+
 define([
-'jquery',
-'underscore',
-'backbone',
-'text!templates/index.html'
-], function ($ , _, Backbone, indexTemplate) {
+	// These are path alias that we configured in our bootstrap
+	'jquery',
+	'underscore',
+	'backbone',
+	'text!templates/index.html',
+	'collections/books'
+], function ($, _, Backbone, IndexTemplate, BookCollection) {
 	var IndexView = Backbone.View.extend({
 		el: $('#content'),
-		render: function () {
-			// Data for _ template
-			var data = {};
-			var compiledTemplate = _.template(indexTemplate, data);
-			// Send template to the page
-			this.$el.html(compiledTemplate);
+		render: function() {
+		  var self = this;
+		  var books = new BookCollection();
+		  books.fetch({
+		    success: function(books) {
+		      // Get all cover links
+		      var coverLinks = [];
+		      books.each(function(book, i) {
+		        coverLinks[i] = book.get('cover');
+		      });
+	        var Template = _.template(IndexTemplate, {coverLinks: coverLinks});
+		    	self.$el.html(Template);
+		    }
+		  });
 		}
 	});
 	return IndexView;
